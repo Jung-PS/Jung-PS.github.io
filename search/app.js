36756b7678192740b04d9ffa0d1111f62c4da3c7
Paponpat Siriruamsap <103345620+Jung-PS@users.noreply.github.com>
2024-06-21 20:39:23 +0700

    Update app.js

--- search/app.js
+++ search/app.js
@@ -57,23 +57,12 @@ var applePayController = (function (uiController) {
         callback(response.data)
       })
   }
-  var _getAvailableShippingMethods = function (region) {
-    // return the shipping methods available based on region
-      return { methods: config.shipping.WORLDWIDE_region }
-  }
-
-  var _calculateTotal = function (subtotal, shipping) {
-    return (parseFloat(subtotal)).toFixed(2)
-  }
   var _performTransaction = function (details, callback) {
     axios
       .post(
         BACKEND_URL_PAY,
         {
           token: details.token,
-          customerEmail: details.shippingContact.emailAddress,
-          billingDetails: details.billingContact,
-          shippingDetails: details.shippingContact
         },
         {
           headers: { 'Access-Control-Allow-Origin': '*' }
@@ -89,64 +78,7 @@ var applePayController = (function (uiController) {
         appleSession.completeMerchantValidation(merchantSession)
       })
     }
-    appleSession.onshippingcontactselected = function (event) {
-      var shipping = _getAvailableShippingMethods(
-        config.shop.shop_localisation.countryCode
-      )
-      var newTotal = {
-        type: 'final',
-        label: config.shop.shop_name,
-        amount: _calculateTotal(
-          config.shop.product_price,
-          shipping.methods[0].amount
-        )
-      }
-      var newLineItems = [
-        {
-          type: 'final',
-          label: 'Subtotal',
-          amount: config.shop.product_price
-        },
-        {
-          type: 'final',
-          label: shipping.methods[0].label,
-          amount: shipping.methods[0].amount
-        }
-      ]
-      appleSession.completeShippingContactSelection(
-        ApplePaySession.STATUS_SUCCESS,
-        shipping.methods,
-        newTotal,
-        newLineItems
-      )
-    }
-    appleSession.onshippingmethodselected = function (event) {
-      var newTotal = {
-        type: 'final',
-        label: config.shop.shop_name,
-        amount: _calculateTotal(
-          config.shop.product_price,
-          event.shippingMethod.amount
-        )
-      }
-      var newLineItems = [
-        {
-          type: 'final',
-          label: 'Subtotal',
-          amount: config.shop.product_price
-        },
-        {
-          type: 'final',
-          label: event.shippingMethod.label,
-          amount: event.shippingMethod.amount
-        }
-      ]
-      appleSession.completeShippingMethodSelection(
-        ApplePaySession.STATUS_SUCCESS,
-        newTotal,
-        newLineItems
-      )
-    }
+   
 
     appleSession.onpaymentauthorized = function (event) {
       _performTransaction(event.payment, function (outcome) {
@@ -175,19 +107,7 @@ var applePayController = (function (uiController) {
             'supportsDebit'
           ],
           supportedNetworks: config.payments.acceptedCardSchemes,
-          shippingType: 'shipping',
-          requiredBillingContactFields: [
-            'postalAddress',
-            'name',
-            'phone',
-            'email'
-          ],
-          requiredShippingContactFields: [
-            'postalAddress',
-            'name',
-            'phone',
-            'email'
-          ],
+
           total: {
             label: config.shop.shop_name,
             amount: config.shop.product_price,
